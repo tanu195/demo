ac7283614fc1615f7ddbda087e3a07883484ee1c
Tanu Mittal <tanu.mittal@metacube.com>
2019-11-12 00:28:36 +0530

    firstcommit

---
+++ demo1/src/demo1/mainDemo.java
@@ -0,0 +1,15 @@
+package demo1;
+
+import java.util.ArrayList;
+import java.util.List;
+
+public class mainDemo {
+	
+	public mainDemo() {
+		List<String> list = new ArrayList<>();
+		list.add("a");
+		list.add("b");
+		System.out.println(list);
+	}
+
+}
