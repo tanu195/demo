db5e93ab164dbd2208b5df8b7f110a8d0c93b687
tanu195 <tanumittal195@gmail.com>
2019-11-12 00:33:49 +0530

    Update mainDemo.java

--- demo1/src/demo1/mainDemo.java
+++ demo1/src/demo1/mainDemo.java
@@ -6,9 +6,11 @@ import java.util.List;
 public class mainDemo {
 	
 	public mainDemo() {
+		System.out.println("hi all");
 		List<String> list = new ArrayList<>();
 		list.add("a");
 		list.add("b");
+		System.out.println("hello all");
 		System.out.println(list);
 	}
 
